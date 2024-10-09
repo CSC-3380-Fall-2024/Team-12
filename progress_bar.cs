2a6ade4d4df5c87f67df844e03db15b69fef9397
Matt-Chowski <Mbalac3@lsu.edu>
2024-10-08 21:58:29 -0500

    Update progress_bar.cs

--- progress_bar.cs
+++ progress_bar.cs
@@ -1,21 +1,29 @@
-using Godot; 
+using Godot;
 using System;
 
-
-# Called when the node enters the scene tree for the first time.
-public class progBar : ProgressBar
+public partial class ProgressBar : Godot.ProgressBar
 {
-
 	int hp = 5; 
-	public ProgressBar healthbar;
+	
+	// Called when the node enters the scene tree for the first time.
 	
 	public override void hpLoseHealth(){
 
 			hp--; 
-			healthbar.value = hp;
+			Godot.ProgressBar = hp;
 	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void hpGainHealth(){
 			hp++;
 			healthbar.value = hp;
 	}
 }
+
+
+
+
+
+
+
+
+
