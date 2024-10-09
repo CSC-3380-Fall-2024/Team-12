5c6673a90b26a3c6ef12c5ac0018f275ea9ce68d
Matt-Chowski <Mbalac3@lsu.edu>
2024-10-08 21:53:38 -0500

    Update progress_bar.cs
    test this to see if it actually changes the progress bar

--- progress_bar.cs
+++ progress_bar.cs
@@ -1,11 +1,21 @@
-extends ProgressBar
+using Godot; 
+using System;
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+public class progBar : ProgressBar
+{
 
+	int hp = 5; 
+	public ProgressBar healthbar;
+	
+	public override void hpLoseHealth(){
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+			hp--; 
+			healthbar.value = hp;
+	}
+	public override void hpGainHealth(){
+			hp++;
+			healthbar.value = hp;
+	}
+}
