262d3f46854d2cc2c00f7bb6137970049a3e8bca
Matt-Chowski <Mbalac3@lsu.edu>
2024-10-15 15:06:17 -0500

    Update progress_bar.cs

--- progress_bar.cs
+++ progress_bar.cs
@@ -5,14 +5,14 @@ public partial class ProgressBar : Godot.ProgressBar
 {
 	int hp = 5; 
 	
-	// Called when the node enters the scene tree for the first time.
+	
 	
 	public void hpLoseHealth(){
 
 			hp--; 
 			this.Value = hp;
 	}
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
 	public void hpGainHealth(){
 			hp++;
 			this.Value = hp;
