9c38d06ad524b3be01fb4acf76399804979d63fd
Aaron <aauco2000@gmail.com>
2024-10-17 16:32:36 -0500

    player flips when changing direction

--- new-game-project/Assets/Scripts/Player.cs
+++ new-game-project/Assets/Scripts/Player.cs
@@ -6,6 +6,12 @@ public partial class Player : CharacterBody2D
 	public const float Speed = 150f;
 	public const float JumpVelocity = -200f;
 
+	public AnimatedSprite2D animatedSprite;
+	
+	public override void _Ready(){
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
@@ -24,7 +30,18 @@ public partial class Player : CharacterBody2D
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
+		
+		//gets input direction: -1, 0, 1
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		
+		//#flip the sprite if need be
+		if (direction.X > 0){
+			animatedSprite.FlipH = false;
+		}
+		else if(direction.X < 0){
+			animatedSprite.FlipH = true;	
+		}
+		
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
