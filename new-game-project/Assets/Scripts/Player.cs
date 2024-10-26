6ca9c529d7da1e31b834652f6b99ad5d03f3a50e
unknown <Mbalac3@lsu.edu>
2024-10-26 14:45:54 -0500

    added the inventory system to be able to change health value according to the armor that is to be picked up

--- new-game-project/Assets/Scripts/Player.cs
+++ new-game-project/Assets/Scripts/Player.cs
@@ -1,12 +1,59 @@
 using Godot;
 using System;
+using System.Net.Http;
 
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 150f;
 	public const float JumpVelocity = -200f;
 
-	public override void _PhysicsProcess(double delta)
+	private ProgressBar health;
+	private int[] healtharray = new int[4];
+	
+	
+
+	//private Area2D attackHitbox;
+
+     public override void _Ready()
+     {
+		health = GetNode<ProgressBar>("Control/ProgressBar");
+		healtharray[1] = 2;
+		hparray();
+    //     attackHitbox = GetNode<Area2D>("AttackHit");
+	// 	attackHitbox.Monitoring = false;
+	// 	attackHitbox.BodyEntered += Onhitbox;
+     }
+	 public void hparray()
+	 {
+		if (healtharray[1] == 0)
+		{
+			health.hp = 10;
+		}else if ( healtharray[1] == 1){
+		health.hp = 20;
+
+	 }else if (healtharray [1] == 2){
+		health.hp = 30;
+	 }
+
+	 health.Value = health.hp;
+	 }
+	 
+	// public void Attack(){
+	// 	attackHitbox.Monitoring = true;
+	// 	SceneTreeTimer attackHitboxtime = GetTree().CreateTimer(0.5f);
+	// 	attackHitboxtime.Timeout += noAttacky;
+		
+	// }
+	// public void noAttacky(){
+	// 	attackHitbox.Monitoring = false;
+	// }
+	// public void Onhitbox(Node body){
+	// 	if (body.HasMethod("hpLoseHealth")){
+	// 		body.Call("hpLoseHealth");
+	// 	}
+	// }
+
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
@@ -36,5 +83,11 @@ public partial class Player : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+
+		// if (Input.IsActionJustPressed("attack")){
+		// 	Attack();
+		// }
+
 	}
+
 }
