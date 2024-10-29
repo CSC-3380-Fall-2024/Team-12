84fa865079cf95619ce92b250f53ad875c91cd05
jackcorson <114191291+jackcorson@users.noreply.github.com>
2024-10-29 15:19:44 -0500

    Update Enemy1.cs
    Making the file conform to the coding standards

--- new-game-project/Assets/Scripts/Enemy1.cs
+++ new-game-project/Assets/Scripts/Enemy1.cs
@@ -1,9 +1,11 @@
 using Godot;
 using System;
 
-public partial class Enemy1 : Node2D
-{	
-	private enum EnemyState{
+public partial class Enemy1 : Node2D {	
+
+	public float detectionRadius = 50f;
+	
+	private enum EnemyState {
 		idle,
 		active
 	}
@@ -12,41 +14,36 @@ public partial class Enemy1 : Node2D
 	
 	private Node2D player;
 	
-	public float detectionRadius = 50f;
-	
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
 		currentState = EnemyState.idle;
 		player = GetNode<Node2D>("../Player");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		if(Position.DistanceTo(player.Position) <= detectionRadius){
+	public override void _Process(double delta) {
+		if(Position.DistanceTo(player.Position) <= detectionRadius) {
 			currentState = EnemyState.active;
 		}
-		else{
+		else {
 			currentState= EnemyState.idle;
 		}
 		
-		if(currentState == EnemyState.idle){
-			handleIdle();
+		if(currentState == EnemyState.idle) {
+			HandleIdle();
 		}
-		else{
-			handleActive(delta);
+		else {
+			HandleActive(delta);
 		}
 	}
 	
-	private void handleIdle(){
+	private void HandleIdle() {
 		GD.Print("IDLE");
 	}
 	
-	private void handleActive(double delta){
+	private void HandleActive(double delta) {
 		GD.Print("ACTIVE");
 		Vector2 direction = (player.Position - Position).Normalized();
 		Position += direction * 100 * (float)delta;
 	}
-	
 }
