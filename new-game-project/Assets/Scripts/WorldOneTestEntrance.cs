9ae67f5622fef7abbc1be4389c79b4d40c0a9ec6
Jack Corson <jackcorson58@gmail.com>
2024-11-02 17:02:42 -0500

    Added the outline for automatically entering a new scene when in a certain area

---
+++ new-game-project/Assets/Scripts/WorldOneTestEntrance.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class WorldOneTestEntrance : Area2D
+{
+
+	private bool entered = false;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) {
+		if (entered == true) {
+			GD.Print("Press any button to enter scene");
+			if (Input.IsAnythingPressed()) {
+				GetTree().ChangeSceneToFile("res://Assets/Nodes/WorldTwoTest.tscn");
+			}
+		}
+	}
+
+	public void _on_area_2d_body_entered(Node2D body) {
+		entered = true;
+	}
+
+	public void _on_area_2d_body_exited(Node2D body) {
+		entered = false;
+	}
+}
