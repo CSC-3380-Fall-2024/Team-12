5018f8433c034db48ea2de321b6c60a07010c53a
Aaron <aauco2000@gmail.com>
2024-11-13 13:19:38 -0600

    Had problems with level backgroumd, need fix, character moves now

--- new-game-project/Assets/Scripts/ProgressBar.cs
+++ new-game-project/Assets/Scripts/ProgressBar.cs
@@ -4,7 +4,7 @@ using System;
 
 public partial class ProgressBar : Godot.ProgressBar
 {
-	int hp = 5;
+	public int hp = 5;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
