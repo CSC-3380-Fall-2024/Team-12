ab5d14897b4ac5d8a1e8a67e5ff7e3148714e802
Aaron <aauco2000@gmail.com>
2024-11-13 12:09:00 -0600

    Added Aaron and Matty-boys code into one file to push

--- new-game-project/Assets/Scripts/Player.cs
+++ new-game-project/Assets/Scripts/Player.cs
@@ -6,10 +6,68 @@ using System.Security.Cryptography.X509Certificates;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 150f;
-	public const float JumpVelocity = -200f;
+	public const float Speed = 150f;              // Max speed of the player
+	public const float JUMP_VELOCITY = -400f;     // Jump velocity
+	public const float GRAVITY = 2000f;            // Gravity force
+	public const float JUMP_DELAY = 0.08f;          // Delay in seconds before jump happens
+	public const float DECELERATION = 600f;        // Deceleration factor
+	public const float ACCELERATION = 800f;        // Acceleration factor
 
-    public override void _PhysicsProcess(double delta)
+
+	private ProgressBar health;
+	private int[] healtharray = new int[4];
+
+	private bool interactable = false;
+
+	private bool interactableCookie = false;
+
+
+	private Timer jumpTimer;
+	private bool jumpRequested = false;
+
+	private AnimatedSprite2D sprite;
+
+
+	public override void _Ready()
+	{
+		//delay timer
+		jumpTimer = new Timer
+		{
+			WaitTime = JUMP_DELAY,
+			OneShot = true
+		};
+		AddChild(jumpTimer);
+		jumpTimer.Timeout += OnJumpTimerTimeout;
+
+		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+
+		health = GetNode<ProgressBar>("Control/ProgressBar");
+		healtharray[1] = 0;
+		hparray();
+
+	}
+
+	public void hparray()
+	{
+		if (healtharray[1] == 0)
+		{
+			health.hp = 5;
+		}
+		else if (healtharray[1] == 1)
+		{
+			health.hp = 10;
+
+		}
+		else if (healtharray[1] == 2)
+		{
+			health.hp = 15;
+		}
+
+		health.Value = health.hp;
+	}
+
+	public override void _PhysicsProcess(double delta)
 	{
 		//input handling
 		float horizontalInput = Input.GetAxis("move_left", "move_right");
@@ -23,19 +81,6 @@ public partial class Player : CharacterBody2D
 			sprite.FlipH = horizontalInput < 0;
 		}
 		else
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		if (Input.IsActionJustPressed("jump") && jumpTimer.IsStopped() && IsOnFloor())
-		{
-			Velocity = new Vector2(Mathf.MoveToward(Velocity.X, targetSpeed, ACCELERATION * (float)delta), velocityY);
-
-			sprite.FlipH = horizontalInput < 0;
-		}
-
-
-		if (!jumpRequested)
 		{
 			Velocity = new Vector2(Mathf.MoveToward(Velocity.X, 0, DECELERATION * (float)delta), velocityY);
 		}
@@ -55,35 +100,74 @@ public partial class Player : CharacterBody2D
 		Velocity = new Vector2(Velocity.X, velocityY);
 		MoveAndSlide();
 
-		if (interactable && Input.IsActionJustPressed("interact")){
-
+		if (interactable && Input.IsActionJustPressed("interact"))
+		{
+			interactableCookie = false;
 			GD.Print("Interacting in zone. Updating health...");
 			healtharray[1] = 1;
 			hparray();
-		}
-	}
-<<<<<<< HEAD
-		public void AreaEntered(Node body){
-			if (body is Player){
-				interactable = true;
-			
-			}
-			
-		}
-		public void AreaExited(Node body){
-			if (body is Player){
 			interactable = false;
-			}
 		}
 
-		// if (Input.IsActionJustPressed("attack")){
-		// 	Attack();
-		// }
+		if (interactableCookie && Input.IsActionJustPressed("food"))
+		{
+			interactable = false;
+			GD.Print("your gay 9000000000");
+			hparray();
+			interactableCookie = false;
 
+		}
+	}
+	public void AreaEnteredHP(Node body)
+	{
+		if (body is Player)
+		{
+			interactableCookie = true;
+
+			GD.Print("your gay 1");
+
+		}
+	}
+
+	public void AreaExitedHP(Node body)
+	{
+		if (body is Player)
+		{
+			interactableCookie = false;
+			GD.Print("your gay 3");
+		}
+	}
+
+	public void AreaEntered(Node body)
+	{
+		if (body is Player)
+		{
+
+			interactable = true;
+			GD.Print("Player entered a general area");
+
+		}
+
+	}
+	public void AreaExited(Node body)
+	{
+		if (body is Player)
+		{
+
+			interactable = false;
+			GD.Print("Player entered a general area");
+		}
 	}
 
 
-=======
+
+	// if (Input.IsActionJustPressed("attack")){
+	// 	Attack();
+	// }
+
+
+
+
 
 	private void OnJumpTimerTimeout()
 	{
@@ -95,4 +179,3 @@ public partial class Player : CharacterBody2D
 		}
 	}
 }
->>>>>>> origin/dev
