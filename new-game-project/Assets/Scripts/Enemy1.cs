449527c5620fe86121613a34eac68f61a143b479
Aaron <aauco2000@gmail.com>
2024-11-16 14:35:21 -0600

    Added Gravity, new states, and future functions to enemy

--- new-game-project/Assets/Scripts/Enemy1.cs
+++ new-game-project/Assets/Scripts/Enemy1.cs
@@ -1,49 +1,97 @@
 using Godot;
 using System;
 
-public partial class Enemy1 : Node2D {	
+public partial class Enemy1 : CharacterBody2D
+{
 
-	public float detectionRadius = 50f;
-	
-	private enum EnemyState {
+	public float detectionRadius = 600f;
+	public float CLOSERANGE = 200f;
+	public float FARRANGE = 500f;
+	[Export]
+	public float speed = 300f;
+	public const float GRAVITY = 2000f;            // Gravity force
+	public const float DECELERATION = 1000f;        // Deceleration factor
+	public const float ACCELERATION = 800f;
+
+	private Random _rng = new Random();
+
+	private enum EnemyState
+	{
 		idle,
-		active
+		active,
+		dance,
+		attack
 	}
-	
+
 	private EnemyState currentState;
-	
+
 	private Node2D player;
-	
+
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() {
+	public override void _Ready()
+	{
 		currentState = EnemyState.idle;
-		player = GetNode<Node2D>("../Player");
+		player = GetNode<Node2D>("../MainCharacter");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
-		if(Position.DistanceTo(player.Position) <= detectionRadius) {
+	public override void _PhysicsProcess(double delta)
+	{
+
+		applyGravity(delta);
+
+
+		if (Position.DistanceTo(player.Position) <= detectionRadius)
+		{
 			currentState = EnemyState.active;
 		}
-		else {
-			currentState= EnemyState.idle;
+		else
+		{
+			currentState = EnemyState.idle;
 		}
-		
-		if(currentState == EnemyState.idle) {
+
+		if (currentState == EnemyState.idle)
+		{
 			HandleIdle();
 		}
-		else {
+		if (currentState == EnemyState.active)
+		{
 			HandleActive(delta);
 		}
+
+		MoveAndSlide();
 	}
-	
-	private void HandleIdle() {
+
+	private void HandleIdle()
+	{
 		GD.Print("IDLE");
 	}
-	
-	private void HandleActive(double delta) {
-		GD.Print("ACTIVE");
+
+	private void HandleActive(double delta)
+	{
+		if (Position.DistanceTo(player.Position) <= CLOSERANGE)
+		{
+			GD.Print("VERY CLOSE");
+		}
+		if (Position.DistanceTo(player.Position) >= FARRANGE)
+		{
+			GD.Print("VERY FAR");
+		}
+
+		GD.Print(Position.DistanceTo(player.Position));
+
 		Vector2 direction = (player.Position - Position).Normalized();
-		Position += direction * 100 * (float)delta;
+		Position += direction * speed * (float)delta;
+	}
+
+	private void dance()
+	{
+
+	}
+
+	public void applyGravity(double delta)
+	{
+		Velocity = new Vector2(Velocity.X, (float)(Velocity.Y + GRAVITY * delta));
+
 	}
 }
